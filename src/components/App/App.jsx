c51c11e20005fb1814fd7d5e599ad782f2a02603
MarynaBublyk <137985327+MarynaBublyk@users.noreply.github.com>
2024-01-21 21:32:54 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,42 +1,51 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import { Container, Title, SubTitle, Wrapper } from './App.styled';
 import ContactForm from '../ContactForm/ContactForm';
 import ContactList from '../ContactList/ContactList';
 import Filter from '../Filter/Filter';
 
-class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+const phoneContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 
-  // Збереження контактів в localStorage
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts'); //Отримуємо дані з localStorage.
-    const parsedContacts = JSON.parse(contacts); // Змінюємо дані зі строки JSON в объект JavaScript.
+const App = () => {
+  // Значення вилучається із localStorage браузера с ключем 'contacts'
+  const [contacts, setContacts] = useState(() => {
+    return JSON.parse(window.localStorage.getItem('contacts')) ?? phoneContacts; //Якщо значення не знайдено, встановлюється значення масива phoneContacts.
+  });
 
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts }); // Вставляємо отримані контакти в об'єкт "contacts".
-    }
-  }
+  const [filter, setFilter] = useState('');
 
-  componentDidUpdate(_, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      // Порівнюємо поточні контакти с попереднім об'єктом контактів.
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-      // Якщо контакти змінились, зберігаємо їх в localStorage.
-    }
-  }
+  // При зміні стану contacts зберігає поточні контакти в localStorage з ключем 'contacts'.
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  // // Збереження контактів в
+  // componentDidMount() {
+  //   const contacts = localStorage.getItem('contacts'); //Отримуємо дані з localStorage.
+  //   const parsedContacts = JSON.parse(contacts); // Змінюємо дані зі строки JSON в объект JavaScript.
+
+  //   if (parsedContacts) {
+  //     this.setState({ contacts: parsedContacts }); // Вставляємо отримані контакти в об'єкт "contacts".
+  //   }
+  // }
+
+  // componentDidUpdate(_, prevState) {
+  //   if (this.state.contacts !== prevState.contacts) {
+  //     // Порівнюємо поточні контакти с попереднім об'єктом контактів.
+  //     localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  //     // Якщо контакти змінились, зберігаємо їх в localStorage.
+  //   }
+  // }
 
   // Додаємо новий контакт в список контактів
-  addContact = contact => {
-    const isInContacts = this.state.contacts.some(
+  const addContact = contact => {
+    const isInContacts = contacts.some(
       ({ name }) =>
         name.toLowerCase().trim() === contact.name.toLowerCase().trim()
     );
@@ -45,19 +54,19 @@ class App extends Component {
       alert(`${contact.name} is already in contacts`);
       return;
     }
-    this.setState(prevState => ({
-      contacts: [{ id: nanoid(), ...contact }, ...prevState.contacts],
-    }));
+    setContacts(prevContacts => [
+      ...prevContacts,
+      { id: nanoid(), ...contact },
+    ]);
   };
 
   // Зміна значення фільтра
-  changeFilter = event => {
-    this.setState({ filter: event.target.value.trim() });
+  const changeFilter = event => {
+    setFilter(event.target.value.trim());
   };
 
   // Отримання відфільтрованих контактів
-  getVisibleContacts = () => {
-    const { filter, contacts } = this.state;
+  const getVisibleContacts = () => {
     const normalizedFilter = filter.toLowerCase();
 
     return contacts.filter(contact =>
@@ -66,41 +75,36 @@ class App extends Component {
   };
 
   // Видалення контакта зі списку
-  removeContact = contactId => {
-    this.setState(prevState => {
-      return {
-        contacts: prevState.contacts.filter(({ id }) => id !== contactId),
-      };
-    });
+  const removeContact = contactId => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== contactId)
+    );
   };
 
-  render() {
-    const visibleContacts = this.getVisibleContacts();
-    const { filter } = this.state;
+  const visibleContacts = getVisibleContacts();
 
-    return (
-      <Container>
-        <Title>Phonebook</Title>
+  return (
+    <Container>
+      <Title>Phonebook</Title>
 
-        <ContactForm onSubmit={this.addContact} />
+      <ContactForm onSubmit={addContact} />
 
-        <SubTitle>Contacts</SubTitle>
-        {this.state.contacts.length > 0 ? (
-          // Фільтр для відображення контактів
-          <Filter value={filter} onChangeFilter={this.changeFilter} />
-        ) : (
-          <Wrapper>Your phonebook is empty. Add first contact!</Wrapper>
-        )}
-        {this.state.contacts.length > 0 && (
-          // Список контактів
-          <ContactList
-            contacts={visibleContacts}
-            onRemoveContact={this.removeContact}
-          />
-        )}
-      </Container>
-    );
-  }
-}
+      <SubTitle>Contacts</SubTitle>
+      {contacts.length > 0 ? (
+        // Фільтр для відображення контактів
+        <Filter value={filter} onChangeFilter={changeFilter} />
+      ) : (
+        <Wrapper>Your phonebook is empty. Add first contact!</Wrapper>
+      )}
+      {contacts.length > 0 && (
+        // Список контактів
+        <ContactList
+          contacts={visibleContacts}
+          onRemoveContact={removeContact}
+        />
+      )}
+    </Container>
+  );
+};
 
 export default App;
